cf58ed4111ed3195b4dac61c4fd77837a93351bb
hie3-336 <tomohiko336@gmail.com>
2024-12-08 18:13:52 +0900

    一旦githubによるログイン回りの設定整備

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,7 @@
 
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
     # You should configure your model like this:
     # devise :omniauthable, omniauth_providers: [:twitter]
 
@@ -28,5 +29,24 @@ module Users
     # def after_omniauth_failure_path_for(scope)
     #   super(scope)
     # end
+
+    # skip_before_action :verify_authenticity_token, only: :github
+
+    def github
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+
+      if @user.persisted?
+        sign_in_and_redirect @user, event: :authentication
+        set_flash_message(:notice, :success, kind: 'github') if is_navigational_format?
+      else
+        session['devise.github_data'] = request.env['omniauth.auth'].except(:extra)
+        redirect_to new_user_registration_url
+      end
+    end
+
+    def failure
+      redirect_to root_path
+    end
+
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,34 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
+         :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable, :omniauthable, omniauth_providers: %i[github]
   validates :name, presence: true
   validates :email, presence: true
   validates :phone_number, presence: true
   validates :birthday, presence: true
 
-  # validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
+  validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
   has_many :tweets, dependent: :destroy
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+      user.telephone_number = '08000000000'
+      user.date_of_birth = '1993-01-01'
+
+      if user.persisted? || auth.provider == 'github'
+        user.skip_confirmation! if auth.provider == 'github'
+        user.save
+      end
+    end
+  end
+  
+
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
+
 end
