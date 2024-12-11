aa447f731cb71d1fa911be07af5c8acc87db9be9
hie3-336 <tomohiko336@gmail.com>
2024-12-11 22:37:59 +0900

    ミス修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   has_many :tweets, dependent: :destroy
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).find_or_create! do |user|
+    where(provider: auth.provider, uid: auth.uid).find_or_create_by! do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
