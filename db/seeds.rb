ba98ed94d08e8e1026cb08f9cc22700a0b2784fa
hie3-336 <tomohiko336@gmail.com>
2024-12-14 16:39:40 +0900

    一旦userのseedを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'csv'
+
+CSV.foreach('db/seeds/user_seed.csv', headers: true) do |row|
+  user = User.create(
+    name: row['name'],
+    email: row['email'],
+    encrypted_password: row['encryoted_password'],
+    phone_number: row['phone_number'],
+    birthday: row['birthday']
+  )
+  # item.image.attach(io: File.open("db/testimage/#{row['filename']}.jpg"), filename: "#{row['filename']}.png")
+  user.save
+end
