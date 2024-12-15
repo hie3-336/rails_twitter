32ae24a45f3fba6c58faeaf9995908f535764424
hie3-336 <tomohiko336@gmail.com>
2024-12-15 10:42:31 +0900

    ツイートのページネーション kaminariで一旦実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,7 @@
 class TweetsController < ApplicationController
   def index
     @user = current_user
-    @tweets = Tweet.with_attached_image
+    @tweets = Tweet.with_attached_image.page(params[:page]).per(5)
+    
   end
 end
