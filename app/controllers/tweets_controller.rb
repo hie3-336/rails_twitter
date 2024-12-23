830875cf55465fdf5319e9b83afff8b03e738d2a
hie3-336 <tomohiko336@gmail.com>
2024-12-23 23:09:10 +0900

    フォローしている人のツイートのみ表示する昨日追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,9 @@ class TweetsController < ApplicationController
   def index
     @user = current_user
     @tweets = Tweet.with_attached_image.page(params[:page]).per(5)
+    if @user.present?
+      @following_tweets = @tweets.where(user_id: @user.followings.map(&:id))
+    end
     
   end
 end
