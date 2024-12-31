eca54daa47873e642b8741eb023d91a9fd932b28
hie3-336 <tomohiko336@gmail.com>
2024-12-31 22:08:18 +0900

    プロフィールページを仮作成

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,3 @@
 //= link_tree ../images
+//= link 660x220.png
 //= link_tree ../builds
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(name: params[:name])
-    @tweets = Tweet.with_attached_image.page(params[:page]).per(5)
+    @user_tweets = Tweet.where(user_id: @user.id).with_attached_image.page(params[:page]).per(5)
 
-    @user_tweets = @tweets.where(user_id: @user.id)
+    # @user_tweets = @user_tweets
   end
 end
