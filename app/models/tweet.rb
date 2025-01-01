2caece361988c29e7dbee3357d48deefca7376e7
hie3-336 <tomohiko336@gmail.com>
2025-01-02 02:26:12 +0900

    いいね、リツイート。コメントのリレーション追記

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,9 @@
 
 class Tweet < ApplicationRecord
   belongs_to :user
+  has_many :like, dependent: :destroy
+  has_many :retweet, dependent: :destroy
+  has_many :comment, dependent: :destroy
 
   # 投稿画像
   has_one_attached :image
