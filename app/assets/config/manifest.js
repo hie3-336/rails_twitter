13fda8bc78984205a7e25b08599898459f658d59
hie3-336 <tomohiko336@gmail.com>
2025-01-04 00:26:43 +0900

    プロフィールページに各種画像が表示されるよう設定

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,4 @@
 //= link_tree ../images
 //= link 660x220.png
+//= link default_i.png
 //= link_tree ../builds
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(name: params[:name])
-    @user_tweets = Tweet.where(user_id: @user.id).with_attached_image.page(params[:page]).per(5)
+    @user_tweets = Tweet.where(user_id: @user.id).with_attached_image.page(params[:page]).per(5).includes([:user])
 
-    @like_tweets = Tweet.where(id: @user.likes.map(&:tweet_id)).with_attached_image.page(params[:page]).per(5)
-    @retweet_tweets = Tweet.where(id: @user.retweets.map(&:tweet_id)).with_attached_image.page(params[:page]).per(5)
-    @comment_tweets = Tweet.where(id: @user.comments.map(&:tweet_id)).with_attached_image.page(params[:page]).per(5)
+    @like_tweets = Tweet.where(id: @user.likes.map(&:tweet_id)).with_attached_image.page(params[:page]).per(5).includes(user: { avater_image_attachment: :blob })
+    @retweet_tweets = Tweet.where(id: @user.retweets.map(&:tweet_id)).with_attached_image.page(params[:page]).per(5).includes([:user])
+    @comment_tweets = Tweet.where(id: @user.comments.map(&:tweet_id)).with_attached_image.page(params[:page]).per(5).includes([:user])
   end
 end
