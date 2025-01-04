bb7fd7957f05801f59129e31876776b716b5dd6a
hie3-336 <tomohiko336@gmail.com>
2025-01-04 18:35:42 +0900

    トップページのツイートプロフィール画像のN+1問題対処

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,9 +3,9 @@
 class TweetsController < ApplicationController
   def index
     @user = current_user
-    @tweets = Tweet.with_attached_image.page(params[:page]).per(5)
+    @tweets = Tweet.with_attached_image.page(params[:page]).per(5).includes(user: { avater_image_attachment: :blob })
     return if @user.blank?
 
-    @following_tweets = @tweets.where(user_id: @user.followings.map(&:id))
+    @following_tweets = @tweets.where(user_id: @user.followings.map(&:id)).page(params[:page]).per(5).includes(user: { avater_image_attachment: :blob })
   end
 end
