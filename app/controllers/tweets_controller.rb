bcade95812b44b50f5338b3d60db8d2ea1595231
hie3-336 <tomohiko336@gmail.com>
2025-01-13 21:42:19 +0900

    tweetsコントローラーの共通処理をbefore_actionに

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,23 +1,16 @@
 # frozen_string_literal: true
 
 class TweetsController < ApplicationController
-  def index
-    @user = current_user
-    @tweet = Tweet.new
-    @tweets = Tweet.with_attached_image.page(params[:page]).per(5).includes(user: { avater_image_attachment: :blob }).order(created_at: :desc)
-    return if @user.blank?
+  before_action :set_user_and_tweets, only: %i[index create]
 
-    @following_tweets = @tweets.where(user_id: @user.followings.pluck(:id)).per(5)
+  def index
+    @tweet = Tweet.new
   end
 
   def create
-    @user = current_user
     @tweet = Tweet.new(tweet_params)
     @tweet.user_id = @user.id
-    @tweets = Tweet.with_attached_image.page(params[:page]).per(5).includes(user: { avater_image_attachment: :blob })
-    return if @user.blank?
 
-    @following_tweets = @tweets.where(user_id: @user.followings.pluck(:id)).per(5)
     if @tweet.save
       redirect_to root_path(tab: 'recommend'), notice: 'ツイートを投稿しました！'
     else
@@ -29,5 +22,13 @@ class TweetsController < ApplicationController
 
   def tweet_params
     params.require(:tweet).permit(:tweet_text, :image)
+  end
+
+  def set_user_and_tweets
+    @user = current_user
+    @tweets = Tweet.with_attached_image.page(params[:page]).per(5).includes(user: { avater_image_attachment: :blob }).order(created_at: :desc)
+    return if @user.blank?
+
+    @following_tweets = @tweets.where(user_id: @user.followings.pluck(:id)).per(5).order(created_at: :desc)
   end
 end
