92be49067979611502e4363dcfdacb0e7c7d4475
hie3-336 <tomohiko336@gmail.com>
2025-02-16 14:02:32 +0900

    ダイレクトメッセージの基本デザインを設定

--- app/controllers/directmessages_controller.rb
+++ app/controllers/directmessages_controller.rb
@@ -6,9 +6,9 @@ class DirectmessagesController < ApplicationController
   end
 
   def show_chatroom
-    @send_user_id = params[:send_user_id]
-    @direct_messages = Directmessage.where(send_user_id: @send_user_id, receive_user_id: current_user.id)
-    .or(Directmessage.where(send_user_id: current_user.id, receive_user_id: @send_user_id))
+    @send_user = User.find_by(id: params[:send_user_id])
+    @direct_messages = Directmessage.where(send_user_id: @send_user.id, receive_user_id: current_user.id)
+    .or(Directmessage.where(send_user_id: current_user.id, receive_user_id: @send_user.id))
     render :index
   end
 
