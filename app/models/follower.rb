033e53d3e2b1cb148734202f771244d0f432d80e
hie3-336 <tomohiko336@gmail.com>
2025-02-27 21:21:50 +0900

    フォローされたときの通知を追加

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -3,4 +3,15 @@
 class Follower < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :followed, class_name: 'User'
+
+  # 通知モデルとのポリモーフィック
+  has_many :notifications, as: :notifiable, dependent: :destroy
+
+  after_create_commit :create_notifications
+
+  private
+  def create_notifications
+    Notification.create(notifiable: self, user: followed)
+  end
+
 end
