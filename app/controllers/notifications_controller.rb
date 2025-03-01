a5a81ba92ea2e1203b6516aef84288f3b441b1e0
hie3-336 <tomohiko336@gmail.com>
2025-03-01 22:38:21 +0900

    通知ページのN+1問題に対応

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,6 +2,6 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = Notification.where(user_id: current_user.id).order(created_at: :desc)
+    @notifications = Notification.where(user_id: current_user.id).order(created_at: :desc).includes(notifiable: [:tweet, user: [avater_image_attachment: :blob]])
   end
 end
