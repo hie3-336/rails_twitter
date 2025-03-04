845368be9077b607804bfd348f5581ee19a70fe4
hie3-336 <tomohiko336@gmail.com>
2025-03-05 01:44:00 +0900

    通知一覧をuserとの関連付けから呼び出すようにする

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,8 +4,8 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = Notification.where(user_id: current_user.id).order(created_at: :desc).includes(notifiable: [:tweet, {
-                                                                                                      user: [avater_image_attachment: :blob]
-                                                                                                    }])
+    @notifications = current_user.notifications.order(created_at: :desc).includes(notifiable: [:tweet, :follower, {
+      user: [avater_image_attachment: :blob]
+    }])
   end
 end
