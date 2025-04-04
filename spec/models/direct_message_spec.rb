77f1167571382af108f03bbbd20eb7eecb5b95be
hie3-336 <tomohiko336@gmail.com>
2025-04-04 22:39:18 +0900

    RSpecのモデルテストで、DirectMessageのモデル名がおかしかったので修正

--- spec/models/direct_message_spec.rb
+++ spec/models/direct_message_spec.rb
@@ -2,6 +2,6 @@
 
 require 'rails_helper'
 
-RSpec.describe Direct_message, type: :model do
+RSpec.describe DirectMessage, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
