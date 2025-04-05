bf23d6b23e93048a292c38d273ae1c2a75febbef
hie3-336 <tomohiko336@gmail.com>
2025-04-05 23:22:44 +0900

    ツイート投稿リクエストスペックの微修正

--- spec/factories/timelines.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :timeline do
-    association :user, factory: :user
-    association :tweet, factory: :tweet
-  end
-end
--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -3,19 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Tweets', type: :request do
-  describe 'POST /tweet' do
-    context "ユーザーがサインインしている状況で" do
-      context "正しい内容のツイートの時" do
-        before do
-          user = FactoryBot.create(:user)
-          sign_in user
-          post tweets_path, params: { tweet: { content: "testのツイートです"} } # 問題の箇所
-        end
-        it "ツイート投稿に成功すること" do
-          expect(response).to redirect_to(root_path(tab: 'recommend'))
-          follow_redirect!
-          expect(response.body).to include("ツイートを投稿しました")
-        end
+  describe 'POST /tweets' do
+    context "ユーザーがサインインしている状況で、正しい内容のツイートの時" do
+      before do
+        user = FactoryBot.create(:user)
+        sign_in user
+        post tweets_path, params: { tweet: { content: "testのツイートです"} }
+      end
+      it "ツイート投稿に成功すること" do
+        expect(response).to redirect_to(root_path(tab: 'recommend'))
+        follow_redirect!
+        expect(response.body).to include("ツイートを投稿しました")
       end
     end
   end
