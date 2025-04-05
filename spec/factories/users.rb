d6beb0ecb2f1a1bca4b6d25fb3b492fcf3b36f87
hie3-336 <tomohiko336@gmail.com>
2025-04-05 12:20:35 +0900

    userのモデルスペックを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :user do
     name { "Makigai" }
-    email { "makigai@test.com" }
+    sequence(:email) { |n| "makigai#{n}@test.com" }
     password { "password" }
     phone_number { "08000000000" }
     birthday { "2024-10-10" }
     confirmed_at { Time.zone.now }
-    # association :tweets
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "有効なユーザーであること" do
+  it "ファクトリで作成したデータは有効なユーザーであること" do
     expect(FactoryBot.build(:user)).to be_valid
+  end
+
+  it "メールアドレスがなければ無効であること" do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("を入力してください")
+  end
+
+  it "名前の文字数が2文字の場合無効であること(最小の境界値)" do
+    user = FactoryBot.build(:user, name: "hi")
+    user.valid?
+    expect(user.errors[:name]).to include("は3文字以上で入力してください")
+  end
+
+  it "名前の文字数が26文字の場合無効であること(最大の境界値)" do
+    user = FactoryBot.build(:user, name: "AmechaYuuchaKonchaKawaiiii")
+    user.valid?
+    expect(user.errors[:name]).to include("は25文字以内で入力してください")
+  end
+
+  it "名前に全角文字を使用した場合無効であること" do
+    user = FactoryBot.build(:user, name: "あめちゃん")
+    user.valid?
+    expect(user.errors[:name]).to include("は不正な値です")
+  end
+
+  it "重複したメールアドレスの場合無効であること" do
+    user1 = FactoryBot.create(:user)
+    user2 = FactoryBot.build(:user, email: user1.email)
+    user2.valid?
+    expect(user2.errors[:email]).to include("はすでに存在します")
   end
 end
