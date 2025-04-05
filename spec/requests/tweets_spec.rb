161461f88408b4104e5dbd78581e6d0fd465fdc6
hie3-336 <tomohiko336@gmail.com>
2025-04-05 23:34:12 +0900

    ツイートのリクエストスペックの異常系を追加

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -8,12 +8,25 @@ RSpec.describe 'Tweets', type: :request do
       before do
         user = FactoryBot.create(:user)
         sign_in user
-        post tweets_path, params: { tweet: { content: "testのツイートです"} }
+        post tweets_path, params: { tweet: {content: "testのツイートです"} }
       end
       it "ツイート投稿に成功すること" do
         expect(response).to redirect_to(root_path(tab: 'recommend'))
         follow_redirect!
         expect(response.body).to include("ツイートを投稿しました")
+      end
+    end
+
+    context "ユーザーがサインインしている状況で、141文字のツイートを投稿した時" do
+      before do
+        user = FactoryBot.create(:user)
+        sign_in user
+        post tweets_path, params: { tweet: {content: "a" * 141} }
+      end
+      it "ツイート投稿に失敗すること" do
+        expect(response).to redirect_to(root_path(tab: 'recommend'))
+        follow_redirect!
+        expect(response.body).to include("ツイートは140文字以内で入力してください")
       end
     end
   end
