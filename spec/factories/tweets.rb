3bafee304b967910f76ea1ecb34c756ba26ef5ed
hie3-336 <tomohiko336@gmail.com>
2025-04-05 10:10:22 +0900

    ユーザーとツイートのファクトリを追加

---
+++ spec/factories/tweets.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :tweet do
+    content {"テストツイートです！"}
+    association :user
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    name { "Makigai" }
+    email { "makigai@test.com" }
+    password { "password" }
+    phone_number { "08000000000" }
+    birthday { "2024-10-10" }
+    confirmed_at { Time.zone.now }
+    # association :tweets
+  end
+end
