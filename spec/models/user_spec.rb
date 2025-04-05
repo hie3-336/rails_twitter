8e8593946afc90945bb7e89bc15e649c9cea8890
hie3-336 <tomohiko336@gmail.com>
2025-04-05 23:15:23 +0900

    モデルスペックのリファクタリング

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,38 +3,50 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "ファクトリで作成したデータは有効なユーザーであること" do
-    expect(FactoryBot.build(:user)).to be_valid
+  context "ファクトリで作成したデータを使用する時" do
+    it "有効なユーザーであること" do
+      expect(FactoryBot.build(:user)).to be_valid
+    end
   end
 
-  it "メールアドレスがなければ無効であること" do
-    user = FactoryBot.build(:user, email: nil)
-    user.valid?
-    expect(user.errors[:email]).to include("を入力してください")
+  context "メールアドレスが無い場合" do
+    let(:user) { FactoryBot.build(:user, email: nil) }
+    it "メールアドレスがなければ無効であること" do
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
   end
 
-  it "名前の文字数が2文字の場合無効であること(最小の境界値)" do
-    user = FactoryBot.build(:user, name: "hi")
-    user.valid?
-    expect(user.errors[:name]).to include("は3文字以上で入力してください")
+  context "名前の文字数が2文字の場合(最小の境界値)" do
+    let(:user) { FactoryBot.build(:user, name: "hi") }
+    it "無効であること" do
+      user.valid?
+      expect(user.errors[:name]).to include("は3文字以上で入力してください")
+    end
   end
 
-  it "名前の文字数が26文字の場合無効であること(最大の境界値)" do
-    user = FactoryBot.build(:user, name: "AmechaYuuchaKonchaKawaiiii")
-    user.valid?
-    expect(user.errors[:name]).to include("は25文字以内で入力してください")
+  context "名前の文字数が26文字の場合(最大の境界値)" do
+    let(:user) { FactoryBot.build(:user, name: "AmechaYuuchaKonchaKawaiiii") }
+    it "無効であること" do
+      user.valid?
+      expect(user.errors[:name]).to include("は25文字以内で入力してください")
+    end
   end
 
-  it "名前に全角文字を使用した場合無効であること" do
-    user = FactoryBot.build(:user, name: "あめちゃん")
-    user.valid?
-    expect(user.errors[:name]).to include("は不正な値です")
+  context "名前に全角文字を使用した場合" do
+    let(:user) { FactoryBot.build(:user, name: "あめちゃん") }
+    it "無効であること" do
+      user.valid?
+      expect(user.errors[:name]).to include("は不正な値です")
+    end
   end
 
-  it "重複したメールアドレスの場合無効であること" do
-    user1 = FactoryBot.create(:user)
-    user2 = FactoryBot.build(:user, email: user1.email)
-    user2.valid?
-    expect(user2.errors[:email]).to include("はすでに存在します")
+  context "重複したメールアドレスを使用した場合" do
+    let(:user1) { FactoryBot.create(:user) }
+    let(:user2) { FactoryBot.build(:user, email: user1.email) }
+    it "重複したメールアドレスの場合無効であること" do
+      user2.valid?
+      expect(user2.errors[:email]).to include("はすでに存在します")
+    end
   end
 end
